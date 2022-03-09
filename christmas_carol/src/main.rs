63f2765485885645fde5f52e07b83ffa7761e181
Ayan Acharya <ayan.acyharya02@gmail.com>
2022-03-10 00:45:51 +0530

    christmas carol

--- christmas_carol/src/main.rs
+++ christmas_carol/src/main.rs
@@ -4,7 +4,26 @@
 
 
 fn main() {
-    
+    let chorus = "On the first day of Christmas\nMy true love sent to me";
 
-    println!("Hello, world!");
+    let lines = ["12 drummers drumming", "Eleven pipers piping",
+                    "Ten lords a-leaping",
+                    "Nine ladies dancing",
+                    "Eight maids a-milking",
+                    "Seven swans a-swimming",
+                    "Six geese a-laying",
+                    "Five golden rings",
+                    "Four calling birds",
+                    "Three French hens",
+                    "Two turtle-doves",
+                    "A partridge in a pear tree"];
+
+    for i in (0..12).rev() {
+        println!("\n{}", chorus);
+
+        for j in i..12 {
+            println!("{}", lines[j]);
+        }
+    }
 }
+
